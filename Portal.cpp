1d38894a0c225858a9b760c90ddaab0dbfd4b57c
HoangMinh666 <111828077+HoangMinh666@users.noreply.github.com>
2022-08-25 16:33:34 +0700

    Update Portal.cpp

--- Portal.cpp
+++ Portal.cpp
@@ -3,7 +3,8 @@
 void Portal::SetPortal(Brick brick)
 {
 	int x, y, kind;
-	for (int i = 0; i < brick.GetNumberOfBrick(); ++i)
+	_numberOfPortal = brick.GetNumberOfBrick();
+	for (int i = 0; i < _numberOfPortal; ++i)
 	{
 		x = brick._brick[i].GetX();
 		y = brick._brick[i].GetY();
@@ -12,6 +13,8 @@ void Portal::SetPortal(Brick brick)
 		if (kind % 3== 0) _portal[i] = Point2D(x, y, ' ');
 		else if (kind % 3== 1) _portal[i] = Point2D(x, y, 'S'); //Item speed up
 		else if (kind % 3== 2) _portal[i] = Point2D(x, y, 'P'); //Item power of bomb up
+
+		_isGot[i] = false;
 	}
 
 }
@@ -30,7 +33,15 @@ int Portal::GetKindOfPortal(Point2D portal)
 	else if (portal.GetC() == 'P') return 2;
 }
 
+int	Portal::GetIdOfPortal(int x, int y)
+{
+	for(int i = 0; i < _numberOfPortal; ++i)
+		if(x == _portal[i].GetX() && y == _portal[i].GetY()) return i;
+}
+
 void Portal::DisplayPortal(Point2D portal)
 {
 	portal.Display();
 }
+
+int Portal::GetNumberOfPortal() { return _numberOfPortal; }
