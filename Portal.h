917c0779d564c0a3804fdd60652e67d281d64c3b
HoangMinh666 <111828077+HoangMinh666@users.noreply.github.com>
2022-08-25 16:33:59 +0700

    Update Portal.h

--- Portal.h
+++ Portal.h
@@ -8,12 +8,15 @@ public:
 	Portal(Brick brick);
 	~Portal();
 	
-	Point2D _portal[MAX_WALL];
+	Point2D _portal[MAX_BRICK];
+	bool	_isGot[MAX_BRICK];
+	int		_numberOfPortal;
 
 	int		GetKindOfPortal(Point2D portal);
+	int		GetNumberOfPortal();
+	int		GetIdOfPortal(int x, int y);
 	void	SetPortal(Brick brick);
 	void	DisplayPortal(Point2D portal);
 private:
 	
 };
-
