224b53c8b0b0dd5162318b396ce905ecfa778257
HoangMinh666 <111828077+HoangMinh666@users.noreply.github.com>
2022-08-26 11:49:18 +0700

    Update Until.h

--- Until.h
+++ Until.h
@@ -5,7 +5,8 @@
 #define MAX_HEIGHT 17 
 #define MAX_WIDTH 17
 #define MAX_WALL 20
-#define MAX_BRICK 100
+#define MAX_BRICK 50
+#define MAX_MONSTER 1
 
 void GoToXY(int x, int y);
 int GetRandomNumber(int maxNum);
