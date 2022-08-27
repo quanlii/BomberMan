1b0aa46a625953f5e83a45d1973e4826b365e97b
HoangMinh666 <111828077+HoangMinh666@users.noreply.github.com>
2022-08-27 11:58:17 +0700

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -1,6 +1,5 @@
 #include "Game.h"
 
-
 GameBomberMan::GameBomberMan()
 {
 	map = new Map2D;
@@ -44,12 +43,15 @@ void GameBomberMan::OnePlayerMode()
 		if (!bomb1->_isExplosion)
 		{
 			if (bomb1->BombExplosion(map, portal, brick, &player1->_bomberMan, monster))
+			{
 				player1->ResetBomberMan(bomb1);
+				GoToXY(17, MAX_HEIGHT);	cout << player1->_liveLeft;
+			}
 		}
 		if (player1->CollideMonster(monster))
 		{
-			player1->ResetBomberMan(bomb1); GoToXY(17, MAX_HEIGHT);
-			cout << player1->_liveLeft;
+			player1->ResetBomberMan(bomb1); 
+			GoToXY(17, MAX_HEIGHT);	cout << player1->_liveLeft;
 		}
 
 		monster->Move(map);
@@ -71,11 +73,90 @@ void GameBomberMan::TwoPlayersMode()
 	system("CLS");
 	bomb1 = new Bomb; bomb2 = new Bomb;
 	player1 = new BomberMan(1, 1, 'X');
-	player2 = new BomberMan(15, 15, 'Y');
+	player2 = new BomberMan(2, 2, 'Y');
 
 	bool finish = false;
+	int scorePlayer1 = 0, scorePlayer2 = 0;
 
 	map->DisplayMap(); player1->Display(); player2->Display();
 	GoToXY(0, MAX_HEIGHT);  cout << "Remaining lives of player 1: " << player1->_liveLeft;
-	GoToXY(0, MAX_HEIGHT + 1);  cout << "Remaining lives of player 2: " << player2->_liveLeft;
+	GoToXY(0, MAX_HEIGHT + 1);  cout << "Score of player 1: " << scorePlayer1;
+	GoToXY(0, MAX_HEIGHT + 2);  cout << "Remaining lives of player 2: " << player2->_liveLeft;
+	GoToXY(0, MAX_HEIGHT + 3);  cout << "Score of player 2: " << scorePlayer2;
+
+	while (1)
+	{
+		if (_kbhit())
+		{
+			char ch = _getch();
+			if (ch == 72 || ch == 80 || ch == 75 || ch == 77) player1->Move(map, ch, bomb1);
+			else if (ch == 'w' || ch == 'a' || ch == 's' || ch == 'd') player2->Move(map, ch, bomb2);
+			else if (ch == 13) player1->SetBomb(bomb1, map);
+			else if (ch == ' ') player2->SetBomb(bomb2, map);
+			else if (ch == 'p') _getch();
+		}
+
+		if(!player1->CheckIsDead())
+		{
+			player1->Display(); 
+			player1->GetPortal(map, bomb1, portal); GoToXY(19, MAX_HEIGHT + 1);  cout << scorePlayer1;
+			if (!bomb1->_isExplosion)
+			{
+				bool isCompetitorDead = false;
+				if (bomb1->BombExplosionMode2(map, portal, brick, &player1->_bomberMan, &player2->_bomberMan, monster, scorePlayer1, isCompetitorDead))
+				{
+					player1->ResetBomberManMode2(bomb1, 1, 1, 'X');
+					GoToXY(29, MAX_HEIGHT);	cout << player1->_liveLeft;
+				}
+				if(isCompetitorDead) 
+				{
+					player2->ResetBomberManMode2(bomb2, 15, 15, 'Y');
+					GoToXY(29, MAX_HEIGHT + 2);	cout << player2->_liveLeft;
+				}
+				GoToXY(19, MAX_HEIGHT + 1);  cout << scorePlayer1;
+			}
+			if (player1->CollideMonster(monster))
+			{
+				player1->ResetBomberManMode2(bomb1, 1, 1, 'X'); 
+				GoToXY(29, MAX_HEIGHT);	cout << player1->_liveLeft;
+			}
+		}
+
+		if(!player2->CheckIsDead())
+		{
+			player2->Display();
+			player2->GetPortal(map, bomb2, portal); GoToXY(19, MAX_HEIGHT + 3);  cout << scorePlayer2;
+			if (!bomb2->_isExplosion && !player2->CheckIsDead())
+			{
+				bool isCompetitorDead = false;
+				if (bomb2->BombExplosionMode2(map, portal, brick, &player2->_bomberMan, &player1->_bomberMan, monster, scorePlayer2, isCompetitorDead))
+				{
+					player2->ResetBomberManMode2(bomb2, 15, 15, 'Y');
+					GoToXY(29, MAX_HEIGHT + 2);	cout << player2->_liveLeft;
+				}
+				if(isCompetitorDead) 
+				{
+					player1->ResetBomberManMode2(bomb1, 1, 1, 'X');
+					GoToXY(29, MAX_HEIGHT);	cout << player1->_liveLeft;
+				}
+				GoToXY(19, MAX_HEIGHT + 3);  cout << scorePlayer2;
+			}
+
+			if (player2->CollideMonster(monster))
+			{
+				player2->ResetBomberManMode2(bomb2, 15, 15, 'Y'); 
+				GoToXY(29, MAX_HEIGHT + 2);	cout << player2->_liveLeft;
+			}
+		}
+		monster->Move(map);
+		monster->Display();
+
+		if (brick->IsDestroyed() || monster->IsDeadAll() || (player2->CheckIsDead() && player1->CheckIsDead()))
+		{
+			finish = true;
+			break;
+		}
+	}
+	system("CLS");
+	cout << scorePlayer1 << " " << scorePlayer2 << endl;
 }
